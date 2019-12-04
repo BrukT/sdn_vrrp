f10b5567236b4d771fd93ed40fc2a0dc0f5c726a
francescobarbarulo <f.barbarulo@studenti.unipi.it>
2019-12-04 09:11:31 -0500

    Added project net

--- mininet/net.py
+++ mininet/net.py
@@ -22,28 +22,41 @@ class NetworkTopo( Topo ):
     "A LinuxRouter connecting two IP subnets"
 
     def build( self, **_opts ):
+	# network A
+        r1 = self.addNode( 'r1', cls=LinuxRouter, ip='10.0.1.1' )
+	r2 = self.addNode( 'r2', cls=LinuxRouter, ip='10.0.1.2' )
 
-        defaultIP = '10.0.1.1/24'  # IP address for r0-eth1
-        router = self.addNode( 'r0', cls=LinuxRouter, ip=defaultIP )
+        h1 = self.addHost( 'h11', ip='10.0.1.3/24', defaultRoute='via 10.0.1.1' )
+        h2 = self.addHost( 'h12', ip='10.0.1.4/24', defaultRoute='via 10.0.1.1' )
+        h3 = self.addHost( 'h13', ip='10.0.1.5/24', defaultRoute='via 10.0.1.1' )
 
-        h1 = self.addHost( 'h1', ip='10.0.1.2/24', defaultRoute='via 10.0.1.1' )
-        h2 = self.addHost( 'h2', ip='10.0.2.2/24', defaultRoute='via 10.0.2.1' )
-        h3 = self.addHost( 'h3', ip='10.0.2.3/24', defaultRoute='via 10.0.2.1' )
+        ss = self.addSwitch('s1')
 
-        s1 = self.addSwitch('s1')
+        self.addLink( ss, r1, intfName2='r1-eth1', params2={ 'ip' : '10.0.1.1/24' } )
+        self.addLink( ss, r2, intfName2='r2-eth1', params2={ 'ip' : '10.0.1.2/24' } )
 
-        self.addLink( h1, router, intfName2='r0-eth1', params2={ 'ip' : '10.0.1.1/24' } )
-        self.addLink( s1, router, intfName2='r0-eth2', params2={ 'ip' : '10.0.2.1/24' } )
+	self.addLink(h1, ss)
+        self.addLink(h2, ss)
+        self.addLink(h3, ss)
 
-        self.addLink(h2, s1)
-        self.addLink(h3, s1)
+	# network B
+	s1 = self.addHost( 'h21', ip='10.0.2.3/24', defaultRoute='via 10.0.2.1' )
+	s2 = self.addHost( 'h22', ip='10.0.2.4/24', defaultRoute='via 10.0.2.1' )
+
+	ls = self.addSwitch('s2')
+
+	self.addLink( ls, r1, intfName2='r1-eth2', params2={ 'ip' : '10.0.2.1/24' } )
+	self.addLink( ls, r2, intfName2='r2-eth2', params2={ 'ip' : '10.0.2.2/24' } )
+
+	self.addLink(s1, ls)
+	self.addLink(s2, ls)
 
 def run():
     "Test linux router"
     topo = NetworkTopo()
     net = Mininet( topo=topo , controller=lambda name: RemoteController( name, ip='127.0.0.1' ))  # controller is used by s1
     net.start()
-    
+
     CLI( net )
     net.stop()
 
