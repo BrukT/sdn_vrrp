4c087b45aaa90073ace8883be5052682ef89f2da
francescobarbarulo <f.barbarulo@studenti.unipi.it>
2019-12-05 06:06:08 -0500

    Added simple mac addresses for debugging + virtual router 10.0.1.10 for floddlight testing

--- mininet/net.py
+++ mininet/net.py
@@ -4,7 +4,6 @@ from mininet.node import Node
 from mininet.log import setLogLevel, info
 from mininet.cli import CLI
 from mininet.node import RemoteController
-#from mininet.nodelib import LinuxBridge
 from mininet.node import OVSBridge
 
 class LinuxRouter( Node ):
@@ -23,13 +22,13 @@ class NetworkTopo( Topo ):
     "Two LinuxRouter connecting two IP subnets"
 
     def build( self, **_opts ):
-	# network A
-        r1 = self.addNode( 'r1', cls=LinuxRouter, ip='10.0.1.1' )
-	r2 = self.addNode( 'r2', cls=LinuxRouter, ip='10.0.1.2' )
+        r1 = self.addNode( 'r1', cls=LinuxRouter, ip='10.0.1.1', mac='00:00:00:00:00:01' )
+	r2 = self.addNode( 'r2', cls=LinuxRouter, ip='10.0.1.2', mac='00:00:00:00:00:02' )
 
-        h1 = self.addHost( 'h11', ip='10.0.1.3/24', defaultRoute='via 10.0.1.1' )
-        h2 = self.addHost( 'h12', ip='10.0.1.4/24', defaultRoute='via 10.0.1.1' )
-        h3 = self.addHost( 'h13', ip='10.0.1.5/24', defaultRoute='via 10.0.1.1' )
+	# network A
+        h1 = self.addHost( 'h11', ip='10.0.1.3/24', mac='00:00:00:00:00:11', defaultRoute='via 10.0.1.10' )
+        h2 = self.addHost( 'h12', ip='10.0.1.4/24', mac='00:00:00:00:00:12', defaultRoute='via 10.0.1.1' )
+        h3 = self.addHost( 'h13', ip='10.0.1.5/24', mac='00:00:00:00:00:13', defaultRoute='via 10.0.1.1' )
 
         ss = self.addSwitch('s1')
 
@@ -41,11 +40,10 @@ class NetworkTopo( Topo ):
         self.addLink(h3, ss)
 
 	# network B
-	s1 = self.addHost( 'h21', ip='10.0.2.3/24', defaultRoute='via 10.0.2.1' )
-	s2 = self.addHost( 'h22', ip='10.0.2.4/24', defaultRoute='via 10.0.2.1' )
+	s1 = self.addHost( 'h21', ip='10.0.2.3/24', mac='00:00:00:00:00:21', defaultRoute='via 10.0.2.1' )
+	s2 = self.addHost( 'h22', ip='10.0.2.4/24', mac='00:00:00:00:00:22', defaultRoute='via 10.0.2.1' )
 
-        # it acts as a normal switch
-	ls = self.addSwitch('s2', cls=OVSBridge)#OVSSwitch)
+	ls = self.addSwitch('s2', cls=OVSBridge) # it acts as a normal switch
 
 	self.addLink( ls, r1, intfName2='r1-eth2', params2={ 'ip' : '10.0.2.1/24' } )
 	self.addLink( ls, r2, intfName2='r2-eth2', params2={ 'ip' : '10.0.2.2/24' } )
